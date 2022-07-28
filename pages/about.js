6576c83a2acfca5c67fea46c03fcf58ac63827bd
Konrad Piegza <piegzini.k@gmail.com>
2022-07-28 16:52:39 +0200

    Create about page
    What's I created is about page. This will be present some information.

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+const about = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Homepage</h1>
+            <p> lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        </div>
+    )
+}
+
+export default about
