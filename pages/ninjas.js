2d541bb04649ef7e367e18022056904205dfef64
Konrad Piegza <piegzini.k@gmail.com>
2022-07-29 15:48:02 +0200

    Add ninjas page.

---
+++ pages/ninjas.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Ninjas = props => {
+    return (
+        <div>
+            <h1> List of all ninjas</h1>
+        </div>
+    );
+};
+
+Ninjas.propTypes = {
+
+};
+
+export default Ninjas;
