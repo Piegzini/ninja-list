e1a6e6325186907440bb87418c30a9a05c161871
Konrad Piegza <piegzini.k@gmail.com>
2022-07-29 17:55:32 +0200

    Add logo and metadata
    - add logo.png and switch it witch header which was a substitution of logo.
    - add meta head component to manage title and other metadata.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
     return (
         <nav>
             <div className="logo">
-                <h1>Ninja list</h1>
+                <Image width={128} height={77} src="/logo.png" alt="Logo"/>
             </div>
             <Link href="/"> Home </Link>
             <Link href="/about"> About </Link>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import styles from '../styles/Home.module.css'
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
-      <div>
-        <h1 className={styles.title}>Welcome</h1>
-          <p className={styles.text}> lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <p className={styles.text}> lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <Link href="/ninjas">
-              <a className={styles.btn}>See ninja listing</a>
-          </Link>
-      </div>
+      <>
+          <Head>
+              <title>Ninja list | Home</title>
+              <meta name="keywords" content="Ninjas" />
+          </Head>
+          <div>
+              <h1 className={styles.title}>Welcome</h1>
+              <p className={styles.text}> lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p className={styles.text}> lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <Link href="/ninjas">
+                  <a className={styles.btn}>See ninja listing</a>
+              </Link>
+          </div>
+
+      </>
 
   )
 }
