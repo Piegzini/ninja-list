dae85625ba15935ff6806f7e5b8cc25859a52f26
Konrad Piegza <piegzini.k@gmail.com>
2022-07-29 15:47:37 +0200

    Add next.js links
    Switched <a> elements to <Link> which is component from next.js framework.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Link from "next/link";
 
 export default function Home() {
   return (
-      <div> Welcome</div>
+      <div>
+        <Navbar />
+        <h1>Welcome</h1>
+          <Link href="/ninjas">
+              <a>See ninja listing</a>
+          </Link>
+        <Footer />
+      </div>
+
   )
 }
